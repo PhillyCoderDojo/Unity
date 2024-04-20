a3742059b8024083267d2416c0bcd0f445ca1510
Sam <74011113+crankyMagician@users.noreply.github.com>
2024-04-20 09:03:44 -0700

    UPDATE: Floors change color when they collide with the ball
    UPDATE: Floors change color when they collide with the ball

--- Assets/Scripts/FallThrough/TileController.cs
+++ Assets/Scripts/FallThrough/TileController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class TileController : MonoBehaviour
 {
     public Material startColour;
+    public Material safeColour;
     Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.sharedMaterial = startColour;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Safe")
+        {
+            rend.sharedMaterial = safeColour;
+        }
     }
 }
