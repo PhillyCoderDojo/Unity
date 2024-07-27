fa4bddc1d25fe9bd64c8a9261c43e94f2abf68c9
Brian Redpath <74011113+crankyMagician@users.noreply.github.com>
2024-07-27 11:36:08 -0400

    UPDATE: setup game manager
    UPDATE: setup game manager

---
+++ Assets/Scripts/MarioClone/GameManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public LevelData[] levels;
+    public Text levelText;
+    public Text starsText;
+    public Text messageText;
+
+    private int currentLevel;
+    private int collectedStars;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (Instance == this)
+        {
+            currentLevel = 0;
+            collectedStars = 0;
+            UpdateUI();
+        }
+    }
+
+    public void CollectStar()
+    {
+        collectedStars++;
+        UpdateUI();
+    }
+
+    public void CompleteLevel()
+    {
+        if (collectedStars >= levels[currentLevel].requiredStars)
+        {
+            messageText.text = "Level Completed!";
+            currentLevel++;
+            collectedStars = 0;
+            if (currentLevel >= levels.Length)
+            {
+                messageText.text = "All Levels Completed!";
+            }
+            else
+            {
+                UpdateUI();
+            }
+        }
+        else
+        {
+            messageText.text = "Not enough stars to complete the level!";
+        }
+    }
+
+    void UpdateUI()
+    {
+        levelText.text = "Level: " + (currentLevel + 1);
+        starsText.text = "Stars: " + collectedStars + "/" + levels[currentLevel].requiredStars;
+        messageText.text = "";
+    }
+}
